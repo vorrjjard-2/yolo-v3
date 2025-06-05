85a606c2c445411ad8b47e1ffaa3f7a70d478df5
vorrjjard <ruben.saulog@gmail.com>
2025-06-05 09:28:00 +0800

    completed dataloader

--- dataset.py
+++ dataset.py
@@ -6,6 +6,8 @@ import torch
 from PIL import Image, ImageFile
 from torch.utils.data import Dataset, DataLoader
 
+from utils import (iou_width_height as iou, non_max_suppression as nms)
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class YOLODataset(Dataset):
@@ -36,3 +38,38 @@ class YOLODataset(Dataset):
     def __getitem___(self, index):
         label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
         bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist()
+        img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
+        image = np.array(Image.open(img_path).convert("RGB"))
+
+        if self.transform:
+            augmentations = self.transform(image=image, bboxes=bboxes)
+            image = augmentations["image"]
+            bboxes = augmentations["bboxes"]
+
+        targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
+
+        for box in bboxes:
+            iou_anchors = iou(torch.tensor(box[2:4]), self.anchors)
+            anchor_indices = iou_anchors.argsort(descending=True, dim=0)
+            x, y, width, height, class_label = box
+            has_anchor = [False, False, False]
+
+            for anchor_idx in anchor_indices:
+                scale_idx = anchor_idx // self.num_anchors_per_scale # 0, 1, 2
+                anchor_on_scale = anchor_idx % self.num_anchors_per_scale
+                S = self.S[scale_idx]
+                i, j = int(S*y), int(S*x)
+                anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
+
+                if not anchor_taken and not has_anchor[scale_idx]: #WHY
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1
+                    x_cell, y_cell = S*x - j, S*y - i
+                    width_cell, height_cell = width*S, height*S
+                    box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
+                    targets[scale_idx][anchor_on_scale, i, j, 1:5] = box_coordinates
+                    targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label)
+
+                elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh:
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = -1
+        
+        return image, tuple(targets)
